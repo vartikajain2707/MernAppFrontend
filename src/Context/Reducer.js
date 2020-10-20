3979c8054e77a48882d890552208869d66223e10
AKSHAT <akthd4@gmail.com>
2020-10-20 11:05:28 +0530

    Context API

---
+++ src/Context/Reducer.js
@@ -0,0 +1,48 @@
+export const initialState = {
+  cart: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD":
+      action.payload.quantity = 1;
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+      };
+    case "PLUS":
+      for (let i = 0; i < state.cart.length; i += 1) {
+        if (state.cart[i].id === action.payload.id) {
+          state.cart[i].quantity += 1;
+        }
+      }
+      return {
+        ...state,
+        cart: [...state.cart],
+      };
+    case "MINUS":
+      for (let i = 0; i < state.cart.length; i += 1) {
+        if (state.cart[i].id === action.payload.id) {
+          if (state.cart[i].quantity === 1) {
+            state.cart.splice(i, 1);
+            return {
+              ...state,
+              cart: [...state.cart],
+            };
+          } else {
+            state.cart[i].quantity -= 1;
+          }
+        }
+      }
+      return {
+        ...state,
+        cart: [...state.cart],
+      };
+    case "REMOVE":
+      return { state };
+    default:
+      return { state };
+  }
+};
+
+export default reducer;
